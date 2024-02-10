d3cf4b628108d282e69713224a4edfcccf992dd1
zebavillegas <zebavillegas@gmail.com>
2024-02-10 03:51:41 +0100

    rutas ok

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 
 @NgModule({
   declarations: [
+    LayoutPageComponent,
+    LoginPageComponent,
     RegisterPageComponent
   ],
   imports: [
--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MoviePageComponent } from './pages/movie-page/movie-page.component';
 
 const routes: Routes = [
   {
@@ -9,7 +11,11 @@ const routes: Routes = [
     path: '',
     component: LayoutPageComponent,
     children: [
-      { path: 'list-page', component: ListPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: ':id', component: MoviePageComponent },
+      { path: '**', redirectTo: 'list' },
+      // TODO: agregar mas rutas hijas
     ]
   }
 ];
