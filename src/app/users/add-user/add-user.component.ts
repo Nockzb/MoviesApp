e22262f6be099f1d77fc65623bcee86e83f7d435
zebavillegas <zebavillegas@gmail.com>
2024-02-12 12:11:12 +0100

    add ok

--- src/app/users/add-user/add-user.component.ts
+++ src/app/users/add-user/add-user.component.ts
@@ -8,7 +8,7 @@ import { User } from 'src/app/shared/interfaces/user.interface';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.scss']
+  styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
   userForm!: FormGroup;
